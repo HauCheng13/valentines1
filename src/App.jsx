feb9e3b05554975ea5fcecc7e7ea11c469789279
Wong Hau Cheng <hc_wong@daikai.com>
2026-02-09 01:49:36 +0800

    damn

--- src/App.jsx
+++ src/App.jsx
@@ -299,7 +299,7 @@ function App() {
         <div className="container" ref={containerRef}>
           <div className="time-jp">{formatTime('Asia/Tokyo')}</div>
           <img src={valentineImg1} alt="Us" className="valentine-img" />
-          <h1>Be my Valentine? 💗</h1>
+          <h1>Valentine Co-worker? (HR Approved)</h1>
           <div className="buttons">
             <button
               className="btn-yes"
