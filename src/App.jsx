bc17d5217487bf6f6a3260b568581e4d77f8b406
Wong Hau Cheng <hc_wong@daikai.com>
2026-02-13 00:54:05 +0800

    dwqdqwd

--- src/App.jsx
+++ src/App.jsx
@@ -330,7 +330,7 @@ function App() {
           <div className="container success-message" style={{ display: 'block' }}>
             <div className="time-jp">{formatTime('Asia/Tokyo')}</div>
             <img src={penguinImg} alt="Penguin" className="penguin-img" />
-            <h1 className="success-text">Heyy Ms.Trang</h1>
+            <h1 className="success-text">Heyy Trang</h1>
             <p className="sub-text">Had some fun with this instead of doing something productive 🤣🤣</p>
             <button className="btn-yes" onClick={handleReset}>Back</button>
           </div>
